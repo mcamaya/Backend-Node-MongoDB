5990c6dc6a3c28fe18b15ffc3cbc9cc4256ba40e
mcamaya <camiamayar@gmail.com>
2023-07-10 11:22:35 -0500

    build: :hammer: Configurando dependencias y conexión a MongoDB

---
+++ Backend-Node-Mongo/config/config.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const conectarDB = async () => {
+    try {
+        const connectionDB = await mongoose.connect(process.env.MONGOURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        const url = `Conectado a MongoDB en el server ${connectionDB.connection.host} - En el puerto ${connectionDB.connection.port}`;
+        console.log(url);
+    } catch (error) {
+        console.log(error.message);
+        process.exit(1);
+    }
+}
+
+export default conectarDB;
---
+++ Backend-Node-Mongo/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import conectarDB from "./config/config.js";
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT;
+
+conectarDB();
+
+app.listen(port, () => {
+    console.log(`Server web listening on port ${port}`);
+});
+
